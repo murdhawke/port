645f34e41117810918e28dbb53bf0fa8f657fe4c
murdhawke <amoscheruiyot22@gmail.com>
2021-02-11 01:11:11 +0300

    Added projects section

---
+++ port/src/components/Contacts.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="contacts">
+        <h3 class="section-title">Contact</h3>
+        <div class="columns is-mobile contact-section">
+            <div class="column is-7 ">
+                <h4 class="subtitle">Feel free to reach out to me!</h4>
+                <b-field>
+                    <b-field>
+                        <b-input placeholder="Name" type="text" ></b-input>
+                    </b-field>
+                </b-field>
+            </div>
+            <div class="column is-5 contacts-form">
+                <figure class="contacts-img">
+                   <img src="../assets/contact-us.svg" alt="Contact Us">
+                </figure> 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+#contacts {
+    min-height: 60vh;
+    max-height: 60vh;
+}
+.contacts-section {
+    margin-right: 50px;
+    margin-left: 50px;
+}
+.contacts-img {
+    margin-right: 100px;
+}
+</style>
--- port/src/views/Home.vue
+++ port/src/views/Home.vue
@@ -3,7 +3,7 @@
     <Fullwidth></Fullwidth>
     <div class="about">
       <div class="column is-half is-offset-3">
-          <h3>About Me</h3>
+          <h3 class="section-title">About Me</h3>
           <p class="aboutme">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
@@ -17,14 +17,16 @@
       </div>
     </div>
     <Projects></Projects>
+    <Contacts></Contacts>
   </div>
 </template>
 
 <script>
 import Fullwidth from '../components/Fullwidth'
 import Projects from '../components/Projects'
+import Contacts from '../components/Contacts'
 export default {
-  components: {Fullwidth, Projects}
+  components: {Fullwidth, Projects, Contacts}
 }
 </script>
 
@@ -35,7 +37,7 @@ export default {
 .about {
   padding-bottom: 100px;
 }
-h3 {
+.section-title {
   font-size: 36px;
   color: #535460;
   font-weight: 500;
